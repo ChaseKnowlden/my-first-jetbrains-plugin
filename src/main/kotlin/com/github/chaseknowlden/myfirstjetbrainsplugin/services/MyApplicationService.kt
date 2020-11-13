2632481d6eb010e44926298f19956e46d317bef9
GitHub Action <action@github.com>
2020-11-13 21:09:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/chaseknowlden/myfirstjetbrainsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chaseknowlden.myfirstjetbrainsplugin.services
+
+import com.github.chaseknowlden.myfirstjetbrainsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/chaseknowlden/myfirstjetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chaseknowlden/myfirstjetbrainsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chaseknowlden.myfirstjetbrainsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chaseknowlden.myfirstjetbrainsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
